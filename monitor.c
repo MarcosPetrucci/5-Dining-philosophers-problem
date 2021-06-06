3925b89ce3743190c24691fc515eb363b1cacfb0
Marcos Pietrucci <marcospietrucci@gmail.com>
2021-06-06 15:57:25 -0300

    Monitor terminado

--- monitor.c
+++ monitor.c
@@ -11,12 +11,6 @@ sem_t mutex;
 //Variavel de condição para os filofos
 typedef struct
 {
-  //Semáforo de cada filofo
-	sem_t sem; //Recebe uma "notificacao" quando o vizinho para de comer
-
-  //Variavel indica se o filosofo está parado esperando para comer
-  int parado;
-
   //Mapeia o estado do filosofo
   enum {PENSANDO,FAMINTO,COMENDO} estado;
 
@@ -26,14 +20,70 @@ filo Filosofo[5];
 //Função que resolve um filosofo ocioso
 void ocioso(int i)
 {
-
-  Filosofo[i].parado = TRUE;
-  
   //Filósofo com fome, mas impossibilitado de comer
   sem_post(&mutex); //Libera mutex
-	
-  //Filosofo aguarda liberação dos palitos do lado
-  sem_wait(&Filosofo[i].sem);
-	
-  Filosofo[i].parado = FALSE;
 }
+
+//Muda o estado do filósofo para comendo se os palitos estiverem disponíveis
+void verifica_disponibilidade(int i)
+{
+  //Verifica se os filosofos do lado estao comendo
+  if(Filosofo[i].estado == FAMINTO && Filosofo[(i+4)%5].estado != COMENDO 
+     && Filosofo[(i+1)%5].estado != COMENDO)
+  {
+    //Os palitos estão disponíveis, comer
+    Filosofo[i].estado = COMENDO;
+  }
+}
+
+void pegar_palitos(int i)
+{
+  //Esperar a disponibiliade do semáforo
+  sem_wait(&mutex);
+
+  //O Filósofo fica faminto
+  Filosofo[i].estado = FAMINTO;
+
+  //Verifica se o filosofo i pode começar a comer
+  verifica_disponibilidade(i);
+
+  while(Filosofo[i].estado == FAMINTO)
+  {
+    //Os palitos não estão disponíveis, então o filosofo esta ocioso
+    ocioso(i);
+  }
+
+  //O filósofo está comendo, incrementar semáforo
+  sem_post(&mutex);
+}
+
+void devolve_palitos(int i)
+{
+  //Esperar pela disponibilidade do semáforo
+  sem_wait(&mutex);
+
+  //O filósofo terminou de comer, 
+  Filosofo[i].estado = PENSANDO;
+
+  //Notifica os filosofos do lado que o atual acabou de comer
+  //Se eles estiverem com fome e for possível, eles vão comer!
+  verifica_disponibilidade((i+4)%5);
+  verifica_disponibilidade((i+1)%5);
+
+}
+
+void iniciar_monitor()
+{
+  //Inicia o semáforo com valor 1
+	sem_init(&mutex,0,1); 
+	
+  int i;
+  for(i = 0;i < 5;i++)
+	{
+    //Inicia todos os filósofos no estado pensando
+		Filosofo[i].estado = PENSANDO;
+	}
+}
+
+
+
